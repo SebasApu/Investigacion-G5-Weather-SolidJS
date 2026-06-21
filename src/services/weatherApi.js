34ac4c789209a719fa7f043074373d7688315ea1
Martin <harold.barquero@ucr.ac.cr>
2026-06-21 12:33:52 -0600

    Elimina espacios en blanco innecesarios en el archivo weatherApi.js

--- src/services/weatherApi.js
+++ src/services/weatherApi.js
@@ -6,13 +6,13 @@ export async function fetchWeather(city) {
     const res = await fetch(
       `${BASE_URL}/weather?q=${encodeURIComponent(city)}&appid=${API_KEY}&units=metric&lang=es`
     );
-    
+
     if (!res.ok) {
       if (res.status === 404) throw new Error('La ciudad ingresada no existe.');
       if (res.status === 401) throw new Error('Tu API Key es inválida o aún se está activando (espera 15-30 mins).');
       throw new Error('Error al obtener datos del clima');
     }
-    
+
     return await res.json();
   } catch (error) {
     // Detectamos si es un fallo de red (sin internet)
